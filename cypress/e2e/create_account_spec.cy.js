769efedf37cec71e9649822e4d76b49146b679dd
yoga <yoga004@brin.go.id>
2023-12-31 13:06:12 +0700

    edit create_account

--- cypress/e2e/create_account_spec.cy.js
+++ cypress/e2e/create_account_spec.cy.js
@@ -55,27 +55,6 @@ describe('Create Account Test', () => {
       // Use Page Object to assert that an error related to duplicate email is displayed
       createAccountPage.getErrorMessages2().should('be.visible');
     });
-
-    it.only('should display an error for weak password', () => {
-      // Get user data from fixture
-      cy.fixture('user_data').then((userData) => {
-        const { firstName, lastName, email } = userData.user;
-        // Use Page Object to visit create account page
-        createAccountPage.visit();
-        // Use Page Object to fill personal information
-        createAccountPage.fillPersonalInformation(firstName, lastName);
-    
-        // Use Page Object to fill sign-in information with the same email
-        // Use a weak password (e.g., a password with less than 8 characters)
-        createAccountPage.fillSignInInformation(email, 'weak');
-    
-        // Use Page Object to submit the form
-        createAccountPage.submitForm();
-    
-        // Use Page Object to assert that an error related to weak password is displayed
-        createAccountPage.getPasswordError().should('be.visible');
-      });
-    });    
   });
   
   it('should display an error for invalid email format', () => {
--- cypress/pages/createAccountPage.js
+++ cypress/pages/createAccountPage.js
@@ -1,49 +1,8 @@
 // cypress/pages/createAccountPage.js
 
 class CreateAccountPage {
-<<<<<<< HEAD
   visit() {
     cy.visit('/customer/account/create/');
-=======
-    visit() {
-      cy.visit('/customer/account/create/');
-    }
-  
-    fillPersonalInformation(firstName, lastName) {
-      cy.get('input[name="firstname"]').type(firstName);
-      cy.get('input[name="lastname"]').type(lastName);
-    }
-  
-    fillSignInInformation(email, password) {
-      cy.get('input[name="email"]').as('emailInput').type(email);
-      cy.get('input[name="password"]').as('passwordInput').type(password);
-      cy.get('input[name="password_confirmation"]').as('confirmPasswordInput').type(password);
-    }
-  
-    submitForm() {
-      cy.get('button.action.submit.primary').click();
-    }
-  
-    getSuccessMessage() {
-      return cy.contains('Thank you for registering with Main Website Store.');
-    }
-  
-    getErrorMessages2() {
-      return cy.get('.message-error > div');
-    }
-
-    getErrorMessages() {
-      return cy.get('.error-message');
-    }
-
-    getPasswordError() {
-      return cy.get('#password-strength-meter');
-    }
-
-    getEmailError() {
-      return cy.get('#email_address-error');
-    }
->>>>>>> main
   }
 
   fillPersonalInformation(firstName, lastName) {
@@ -52,9 +11,9 @@ class CreateAccountPage {
   }
 
   fillSignInInformation(email, password) {
-    cy.get('input[name="email"]').type(email);
-    cy.get('input[name="password"]').type(password);
-    cy.get('input[name="password_confirmation"]').type(password);
+    cy.get('input[name="email"]').as('emailInput').type(email);
+    cy.get('input[name="password"]').as('passwordInput').type(password);
+    cy.get('input[name="password_confirmation"]').as('confirmPasswordInput').type(password);
   }
 
   submitForm() {
@@ -65,8 +24,20 @@ class CreateAccountPage {
     return cy.contains('Thank you for registering with Main Website Store.');
   }
 
+  getErrorMessages2() {
+    return cy.get('.message-error > div');
+  }
+
   getErrorMessages() {
     return cy.get('.error-message');
+  }
+
+  getPasswordError() {
+    return cy.get('#password-strength-meter');
+  }
+
+  getEmailError() {
+    return cy.get('#email_address-error');
   }
 }
 
